2bd0ec6de921f0e5be5e5ec087b8494fab93ae29
Nazmul Dewan <nazmul.dewan58@myhunter.cuny.edu>
2025-05-02 09:28:03 -0400

    Require Fields For Campus Form

--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -52,22 +52,22 @@ const NewCampusView = (props) => {
           </div>
           <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
             <label style= {{color:'#11153e', fontWeight: 'bold'}}>Campus ID: </label>
-            <input type="text" name="campusId" onChange ={(e) => handleChange(e)} />
+            <input type="text" name="campusId" onChange ={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Campus Name: </label>
-            <input type="text" name="name" onChange={(e) => handleChange(e)} />
+            <input type="text" name="name" onChange={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}> Address: </label>
-            <input type="text" name="address" onChange={(e) => handleChange(e)} />
+            <input type="text" name="address" onChange={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}> Description: </label>
-            <input type="text" name="description" onChange={(e) => handleChange(e)} />
+            <input type="text" name="description" onChange={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
