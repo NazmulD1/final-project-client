84e812fb20e61fd21f0a6d37e6ab7db63cfad935
Nazmul Dewan <nazmul.dewan58@myhunter.cuny.edu>
2025-05-04 21:42:58 -0400

    Campus Image Shown When No Students Exist

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -13,6 +13,13 @@ const CampusView = (props) => {
   if (campus.students.length === 0) {
     return (
       <div>
+      <h1>{campus.name}</h1>
+      
+      <img 
+        src={campus.url} 
+        alt={campus.name} 
+      />
+      
         <p style={{ fontSize: '30px' }}>There are no students attending this school.</p>
         <Link to={`newstudent`}>
           <button style={{ fontSize: '20px' }}>Add New Student</button>
@@ -33,10 +40,10 @@ const CampusView = (props) => {
       <p>{campus.description}</p>      
 
 
-        <img 
-          src={campus.url} 
-          alt={campus.name} 
-        />
+      <img 
+        src={campus.url} 
+        alt={campus.name} 
+      />
       
 
       <button onClick={() => deleteCampus(campus.id)}>Delete</button> 
