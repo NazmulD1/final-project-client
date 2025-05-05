54141f352459a6f49c73f961dff142d52024b84e
Nazmul Dewan <nazmul.dewan58@myhunter.cuny.edu>
2025-05-04 22:13:36 -0400

    Student View Shows All Data Fields & Campus Hyperlink

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,19 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+
+/*
+7. can navigate to the Single Student View, and
+○ see details about a single student, with all data fields, including the campus at which the student
+is enrolled (if exists)
+○ see an informative message if the student is not enrolled at a campus
+○ navigate to the Single Campus View of the student's enrolled campus
+○ navigate to the Edit Student View and edit the student's information
+○ delete the student (e.g., via link/button and optionally, this can be part of the All Students View)
+*/
+
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -11,8 +24,10 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
-      <h3>{student.gpa}</h3>
+
+      <Link to={`/campus/${student.campus.id}`}> <h2>{student.campus.name}</h2> </Link>
+          
+      <h3>GPA: {student.gpa}</h3>
       <h3>{student.email}</h3>
 
       <img 
