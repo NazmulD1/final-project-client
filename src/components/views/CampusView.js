a4d52d6f74b53c0d0c76e19bcb0c28957224b3a4
bobawanna <evasea85@gmail.com>
2025-05-16 01:43:17 -0400

    Fixed campus deletion on previous commit

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -2,7 +2,8 @@
 CampusView.js
 
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
-It constructs a React component to display a single campus and its students (if any).
+It constructs a React component to display a single campus and its students (if any). 
+Fixed deletion
 ================================================== */
 import { Link } from "react-router-dom";
 
